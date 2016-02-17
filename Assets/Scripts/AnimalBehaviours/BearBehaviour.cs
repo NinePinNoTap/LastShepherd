7999bacd26def67ef53837a0568660903e1f33a0
unknown <1100484@MC10801.uad.ac.uk>
2016-02-17 16:56:43 +0000

    Fixed guard

--- Assets/Scripts/AnimalBehaviours/BearBehaviour.cs
+++ Assets/Scripts/AnimalBehaviours/BearBehaviour.cs
@@ -3,14 +3,35 @@ using System.Collections;
 
 public class BearBehaviour : AnimalBehaviour
 {
+    [Header("Bear Behaviour")]
+    public float attackRange = 2.0f;
+    public SphereCollider sphereCollider;
+
     void Start()
     {
-	
+        // Create a sphere for the attack range
+        sphereCollider.radius = attackRange;
     }
 	
-    void Update()
+    void OnTriggerEnter(Collider col)
     {
-	
+        // Ignore self
+        if(col.gameObject.Equals(this.gameObject))
+        {
+            return;
+        }
+
+        // Ignore triggers
+        if(col.isTrigger)
+            return;
+
+        // Check for a guard
+        if(col.gameObject.tag == "Guard")
+        {
+            // ROAR
+            Debug.Log("ROAR - DIE!");
+            Destroy(col.gameObject);
+        }
     }
 }
 
