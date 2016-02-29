24ad5874daf0e4c0fa7b511e8507ea7e820bdbd8
NinePinNoTap <1100484@live.abertay.ac.uk>
2016-02-29 19:32:49 +0000

    Renamed scripts to fit with new animals. Added a core turtle script.

---
+++ Assets/Scripts/Animal/TurtleBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurtleBehaviour : MonoBehaviour
+{
+	public enum TurtleState { ONBACK, NORMAL };
+
+	[Header("Turtle Behaviour")]
+	public TurtleState turtleState;
+
+	void Start ()
+	{
+		turtleState = TurtleState.NORMAL;
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	public bool IsOnBack()
+	{
+		return turtleState.Equals(TurtleState.ONBACK);
+	}
+}
+
