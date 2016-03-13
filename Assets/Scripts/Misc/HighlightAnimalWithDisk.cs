4d1854289ec32758e581e86e997fe91c31083538
NinePinNoTap <1100484@live.abertay.ac.uk>
2016-03-13 18:57:41 +0000

    Added two solutions for animal highlighting. Both scripts are added to GameManager in level1. Have one script active at a time.

---
+++ Assets/Scripts/Misc/HighlightAnimalWithDisk.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightAnimalWithDisk : MonoBehaviour
+{
+    [Header("Component")]
+    public StackManager stackManager;
+
+    [Header("Highlighting")]
+    public GameObject highlightDisk;
+    public Color frogColor = Color.red;
+    public Color monkeyColor = Color.green;
+    public Color penguinColor = Color.blue;
+    public Color turtleColor = Color.black;
+
+    void Start ()
+    {
+    }
+
+    void Update ()
+    {
+        int index;
+
+        index = stackManager.gameAnimals.IndexOf(stackManager.currentAnimal);
+
+        // Get animal color
+        switch(index)
+        {
+            case 0:
+                highlightDisk.GetComponent<SpriteRenderer>().color = frogColor;
+                frogColor.a = 0.5f;
+                break;
+
+            case 1:
+                highlightDisk.GetComponent<SpriteRenderer>().color = monkeyColor;
+                monkeyColor.a = 0.5f;
+                break;
+
+            case 2:
+                highlightDisk.GetComponent<SpriteRenderer>().color = penguinColor;
+                penguinColor.a = 0.5f;
+                break;
+
+            case 3:
+                highlightDisk.GetComponent<SpriteRenderer>().color = turtleColor;
+                turtleColor.a = 0.5f;
+                break;
+        }
+
+        highlightDisk.transform.position = stackManager.currentStack.Get(0).transform.position - 
+                                            new Vector3(0.0f, (stackManager.animalHeight / 2), 0.0f) +
+                                            new Vector3(0.0f, 0.01f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/Misc/HighlightAnimalWithLine.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightAnimalWithLine : MonoBehaviour
+{
+    [Header("Component")]
+    public StackManager stackManager;
+
+    [Header("Highlighting")]
+    public Color frogColor = Color.red;
+    public Color monkeyColor = Color.green;
+    public Color penguinColor = Color.blue;
+    public Color turtleColor = Color.black;
+    public Material material;
+
+    public LineRenderer lineRenderer;
+
+	void Start ()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.SetWidth(.5f, .5f);
+	}
+	
+	void Update ()
+    {
+        int index;
+
+        index = stackManager.gameAnimals.IndexOf(stackManager.currentAnimal);
+
+        // Get animal color
+        switch(index)
+        {
+            case 0:
+                frogColor.a = 0.5f;
+                lineRenderer.SetColors(frogColor, frogColor);
+                break;
+
+            case 1:
+                monkeyColor.a = 0.5f;
+                lineRenderer.SetColors(monkeyColor, monkeyColor);
+                break;
+
+            case 2:
+                penguinColor.a = 0.5f;
+                lineRenderer.SetColors(penguinColor, penguinColor);
+                break;
+
+            case 3:
+                turtleColor.a = 0.5f;
+                lineRenderer.SetColors(turtleColor, turtleColor);
+                break;
+        }
+
+        Vector3 animalPos = stackManager.currentAnimal.transform.position;
+        lineRenderer.SetPosition(0, animalPos);
+        animalPos.y = 20.0f;
+        lineRenderer.SetPosition(1, animalPos);
+        lineRenderer.material = material;
+	}
+}
