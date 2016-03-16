8ffc9e1d1c0a8e7a44f08cdccd5e23905bd858c2
NinePinNoTap <1100484@live.abertay.ac.uk>
2016-03-16 11:51:23 +0000

    Added level overlay. To edit the script, change the activate key (already set to LB). Set isactivated to false to start so only shows when press the button.

---
+++ Assets/Scripts/Misc/ToggleMonobehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ToggleMonobehaviour : MonoBehaviour 
+{
+    public KeyCode activateKey;
+    public bool isShowing = true;
+    public MonoBehaviour componentToggle;
+
+	void Start ()
+    {
+        componentToggle.enabled = isShowing;
+	}
+	
+	void Update ()
+    {
+        // Check for input
+        if(Input.GetKeyDown(activateKey))
+        {
+            // Reverse the flag
+            isShowing = !isShowing;
+
+            componentToggle.enabled = isShowing;
+
+            Time.timeScale = (Time.timeScale * -1) + 1;
+        }
+	}
+}
